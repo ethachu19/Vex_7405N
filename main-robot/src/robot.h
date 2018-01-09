9bc32f2a8ecd5b2ee9bf8a2f29fe3d4174d9a93e
Ethan Mak <ethan.mak100@gmail.com>
2018-01-08 21:33:06 -0500

    The war has started

---
+++ main-robot/src/robot.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Robot {
+public:
+private:
+};
+
+extern Robot robot;
+
+const int LEFT_DRIVE = 5;
+const int RIGHT_DRIVE = 2; //needs to be reversed
+const int LEFT_LIFT_MOTOR = 3;
+const int RIGHT_LIFT_MOTOR = 6; //needs to be reversed
+const int CLAW = 4;
+const int CHAINBAR = 9;
+const int MOBILE_LIFT = 10;
+const int RIGHT_LIFT_POT = 1;
+const int LEFT_LIFT_POT = 2;
+
+const int LEFT_IME = 0;
+const int RIGHT_IME = 2;
